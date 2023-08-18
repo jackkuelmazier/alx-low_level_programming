16e16dc1c81cc039fcd11260c8898fe31612d49c
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-08-18 08:05:37 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,9 @@ void print_square(int size)
 	int row, column;
 
 	for (row = 1; row < size; row++)
+		_putchar('\n');
 	{
 		for (column = 1; column < size; column++)
 			_putchar('#');
 	}
-	_putchar('\n');
 }
