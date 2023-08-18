132fd6199de23467b4bbe5377a7a0a8306c8a292
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-08-18 06:44:06 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,8 +16,8 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar('_');
+			for (j = 1; j < i; j++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
