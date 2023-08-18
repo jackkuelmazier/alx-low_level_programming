cab82846aad911e4d026725bf571a1b5cb6d22f3
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-08-17 19:46:34 -0700

    no 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * print_square - print a square
+ *
+ * @size: size of a square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
