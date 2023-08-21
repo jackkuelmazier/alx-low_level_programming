cdb33f9569d243d302408d4c24971cfe98fe3d37
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-08-21 13:52:11 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to convert
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int c = 0;
+	int min = 1;
+
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	min *= -1;
+	}
+	while (s[c] >= 48 && s[c] <= 57)
+	}
+}
