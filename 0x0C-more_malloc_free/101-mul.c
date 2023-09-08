dc938e2ae083e5a44e55040144318177751b6281
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-09-07 18:57:54 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,42 +1,96 @@
+#include "main.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ * print_string - takes a pointer to a string
+ * @str: string
+ * Return: nothing
+ */
+void print_string(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
 
+/**
+ * print_error - prints an error message
+ * Return: nothing
+ */
+
+void print_error(void)
+{
+	print_string("Error\n");
+	exit(98);
+}
+
+/**
+ * string_to_int - prints the character
+ * @str: string
+ * Return: result
+ */
+int string_to_int(char *str)
+{
+	int result = 0;
+
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			print_error();
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result);
+}
+
+/**
+ * int_to_string - takes an integer and a pointer
+ * @num: number of integers
+ * @buffer: buffer
+ * Return: nothing
+ */
+void int_to_string(int num, char *buffer)
+{
+	int i;
+	int length = 0;
+
+	while (num)
+	{
+		buffer[length++] = (num % 10) + '0';
+		num /= 10;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = buffer[i];
+
+		buffer[i] = buffer[length - i - 1];
+		buffer[length - i - 1] = temp;
+	}
+	buffer[length] = '\0';
+}
 /**
  * main - multiplies two positive numbers
  * @argc: integer that represents the number of arguments
  * @argv: array of pointers to strings
  * Return: 0
  */
-
 int main(int argc, char **argv)
 {
-	int j, i, num1, num2, result;
+	int num1, num2, result;
 	char buffer[100];
 
 	if (argc != 3)
-	{
-		write(1, "Error\n", 6);
-		exit(98);
-	}
-	for (i = 1; i <= 2; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				write(1, "Error\n", 6);
-				exit(98);
-			}
-		}
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+		print_error();
+
+	num1 = string_to_int(argv[1]);
+	num2 = string_to_int(argv[2]);
 	result = num1 * num2;
 
-	sprintf(buffer, "%d", result);
-	write(1, buffer, strlen(buffer));
-	write(1, "\n", 1);
+	int_to_string(result, buffer);
+	print_string(buffer);
+	print_string("\n");
 	return (0);
 }
