65ad9f77806e39826cfe641bcd660f2d4fcac6e5
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-10-02 14:54:04 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,55 +3,49 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and prints
  * it to the POSIX standard output
  * @filename: pointer to a constant character
- * @letters: number of letters that should
- * be read and printed
- * Return: the actual number of letters it
- * could read and print
+ * @letters: number of letters
+ * Return: the actual number of letters
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = 0;
-	char *buffr = malloc(sizeof(char) * letters);
-	ssize_t read_bytes = read(fd, buffr, letters);
-	ssize_t writ_bytes = write(STDOUT_FILENO, buffr, read_bytes);
+	ssize_t read_bytes, writ_bytes;
+	char *buffr;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
 		return (0);
 	}
-
+	buffr = malloc(sizeof(char) * letters);
 	if (buffr == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
+	read_bytes = read(fd, buffr, letters);
 	if (read_bytes == -1)
 	{
 		free(buffr);
 		close(fd);
 		return (0);
 	}
-
-	if (writ_bytes == -1 || writ_bytes != read_bytes)
-	{
-		free(buffr);
-		close(fd);
-		return (0);
-	}
-
+	writ_bytes = write(STDOUT_FILENO, buffr, read_bytes);
+		if (writ_bytes == -1 || writ_bytes != read_bytes)
+		{
+			free(buffr);
+			close(fd);
+			return (0);
+		}
 	free(buffr);
 	close(fd);
 
