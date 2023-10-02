7a0f009314023b83b15af62c968703718ed81559
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-10-02 16:55:10 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define BUFFER_SIZE 1024
+/**
+ * main - copies the content of a file to another file
+ * @argc: an integer
+ * @argv: an array of character pointers
+ * Return: 0
+ */
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	do {
+		bytes_read = read(file_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(file_from);
+			close(file_to);
+			exit(98);
+		}
+		if (bytes_read > 0)
+		{
+			bytes_written = write(file_to, buffer, bytes_read);
+			if (bytes_written == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close(file_from);
+				close(file_to);
+				exit(99);
+			}
+		}
+	} while (bytes_read > 0);
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
