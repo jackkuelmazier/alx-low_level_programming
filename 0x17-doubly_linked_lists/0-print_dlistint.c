329567901a1387cbfb049fb17592cbeeba9907a7
jacklham <jackkuelmazierlham@gmail.com>
2023-11-09 14:54:51 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        nodes++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
 
-    return nodes;
+	return (nodes);
 }
